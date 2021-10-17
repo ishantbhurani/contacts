7b3c78e03c52b525d11f1969e59cfeb141efcfc0
Ishant Bhurani <ishantbhurani@gmail.com>
2021-10-17 20:55:06 +0530

    Add logic to show modal when contact item clicked

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
 const optionsButtons = document.querySelectorAll(".options-btn");
+const contactItems = document.querySelectorAll(".contact");
+const modal = document.querySelector(".modal");
+const modalForm = modal.querySelector("form");
 
 optionsButtons.forEach((optBtn) =>
   optBtn.addEventListener("click", function () {
     this.parentNode.classList.toggle("show-menu");
     this.blur();
+  })
+);
+
+contactItems.forEach((contact) =>
+  contact.addEventListener("click", function () {
+    modal.classList.add("show");
   })
 );
